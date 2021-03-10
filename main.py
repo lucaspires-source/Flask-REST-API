a705c905b0a1f4ee97cff2ef832418a8ae7a0b0e
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-03-10 15:06:29 -0300

    passando argumentos

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from flask_restful import Api,Resource
 app = Flask(__name__)
 api = Api(app)
 
-
+names = {"Lucas":{"age":26,"gender":male}}
 class HelloWorld(Resource):
-    def get(self):
-        return{"data": "Olá mundo"}
-api.add_resource(HelloWorld, "/helloword")
+    def get(self, name):
+        return{"data": name}
+    def post(self):
+        return{"data": "postado"}
+api.add_resource(HelloWorld, "/helloword<string:name>")
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "HTTP://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld")
+response = requests.get(BASE + "helloworld/tim/19")
 print(response.json())
