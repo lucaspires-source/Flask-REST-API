dc13a298535b43bfc8eb657b241538d82c4e05bf
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-03-10 16:01:42 -0300

    mandando mensagem de statys

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ class Video(Resource):
         return videos[video_id]
     def put(self, video_id):
         args =video_put_args.parse_args()
-        return{video_id:args}
+        videos[video_id] = args
+        return videos[video_id],201
 
 api.add_resource(Video, "/video<int:video_id>")
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -2,5 +2,8 @@ import requests
 
 BASE = "HTTP://127.0.0.1:5000/"
 
-response = requests.put(BASE + "/video",{"likes":10})
+response = requests.put(BASE + "video/1",{"likes":10,"name":"Lucas","views":1000})
+print(response.json())
+input()
+response = requests.get(BASE + "video/1")
 print(response.json())
