7afa1e77d60b410e61e8a86cf2578e53cf188141
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-03-10 14:48:47 -0300

    adicionand resources

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ from flask_restful import Api,Resource
 app = Flask(__name__)
 api = Api(app)
 
+
+class HelloWorld(Resource):
+    def get(self):
+        return{"Olá mundo"}
+api.add_resource(HelloWorld, "/helloword")
 if __name__ == "__main__":
     app.run(debug=True)
