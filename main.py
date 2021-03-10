ebcd99dae8384cf9b7dd4b9611c9a1f51f802cac
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-03-10 15:32:38 -0300

    adcionando dados na memória

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api,Resource
 
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {"Lucas":{"age":26,"gender":male}}
-class HelloWorld(Resource):
-    def get(self, name):
-        return{"data": name}
-    def post(self):
-        return{"data": "postado"}
-api.add_resource(HelloWorld, "/helloword<string:name>")
+
+videos ={}
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+    def put(self, video_id):
+        print(request.form['likes'])
+        return{}
+
+api.add_resource(Video, "/video<int:video_id>")
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "HTTP://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld/tim/19")
+response = requests.put(BASE + "/video",{"likes":10})
 print(response.json())
